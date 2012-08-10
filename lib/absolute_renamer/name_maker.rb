3075b5ea6f5f7ca0027d9662965bc39c26b9c07f
Simon COURTOIS <scourtois@cubyx.fr>
2012-08-10 11:42:30 +0200

    Adding get/set on NameMaker#interpreters and removing useless config argument

--- lib/absolute_renamer/name_maker.rb
+++ lib/absolute_renamer/name_maker.rb
@@ -1,16 +1,15 @@
 module AbsoluteRenamer
   class NameMaker
-    attr_writer :interpreters
+    attr_accessor :interpreters
 
-    def initialize(config, interpreters)
-      @config       = config
-      @interpreters = interpreters
+    def initialize(interpreters = [])
+      self.interpreters = interpreters
     end
 
     def new_value_for(part_name, path_handler)
       new_value = path_handler.send(:"new_#{part_name}")
 
-      @interpreters.inject(new_value) do |value, interpreter|
+      interpreters.inject(new_value) do |value, interpreter|
         interpreter.search_and_replace part_name, value, path_handler
       end
     end
