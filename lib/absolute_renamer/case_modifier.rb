ecc1a6abf2ba3d6034c3fe5823898a9ea4ddb0fe
Simon COURTOIS <scourtois@cubyx.fr>
2012-08-10 12:19:11 +0200

    Refactoring CaseModifier specs and documenting the code

--- lib/absolute_renamer/case_modifier.rb
+++ lib/absolute_renamer/case_modifier.rb
@@ -1,5 +1,23 @@
 module AbsoluteRenamer
+  # Provides case modification methods.
   module CaseModifier
+
+    # Modifies the case of a string based on a modifier character.
+    #
+    # string   - The String to modify.
+    # modifier - The modifier character.
+    #
+    # Examples
+    #
+    #   modify_case "hello world", "*" #=> "Hello World"
+    #   modify_case "hello world", "&" #=> "HELLO WORLD"
+    #   modify_case "Hello World", "%" #=> "hello world"
+    #
+    #   Any other modifier will simply have no effect:
+    #
+    #   modify_case "wello world", "x" #=> "hello world"
+    #
+    # Returns a copy the string with modified case.
     def modify_case(string, modifier)
       case modifier
       when '*' then camelcase string
@@ -9,6 +27,16 @@ module AbsoluteRenamer
       end
     end
 
+    # Change the case of a string to camelcase.
+    #
+    # string - The string to camelcase.
+    #
+    # Examples
+    #
+    #   camelcase "hello world" #=> "Hello World"
+    #   camelcase "heLLo wOrLd" #=> "Hello World"
+    #
+    # Returns a camelcase copy of the string.
     def camelcase(string)
       string.downcase
             .gsub(/(\b|([^a-zA-Z0-9']))([a-z])/) { "#{$2}" + $3.capitalize }
--- spec/case_modifier_spec.rb
+++ spec/case_modifier_spec.rb
@@ -1,38 +1,32 @@
-require File.expand_path('../spec_helper', __FILE__)
 require 'absolute_renamer/case_modifier'
 
 module AbsoluteRenamer
   describe CaseModifier do
-    before :each do
-      @dummy = Object.new
-      @dummy.extend CaseModifier
-      @str = 'hEllO WorlD'
-    end
+    subject { Object.new.extend CaseModifier }
+    let(:string) { 'hEllO WorlD' }
 
-    context "using the modify_case method" do
-      it "should return its first parameter uppercased when the modifier is &" do
-        @dummy.modify_case(@str, '&').should eq('HELLO WORLD')
+    describe '#modify_case' do
+      it "should return its first parameter uppercased when the modifier is '&'" do
+        subject.modify_case(string, '&').should eq('HELLO WORLD')
       end
 
-      it "should return its first parameter downcased when the modifier is %" do
-        @dummy.modify_case(@str, '%').should eq('hello world')
+      it "should return its first parameter downcased when the modifier is '%'" do
+        subject.modify_case(string, '%').should eq('hello world')
       end
 
-      it "should return its first parameter capitalized when the modifier is *" do
-        @dummy.modify_case(@str, '*').should eq('Hello World')
-      end
-
-      it "should return its first parameter unchanged when the modifier is $" do
-        @dummy.modify_case(@str, '$').should eq(@str)
+      it "should return its first parameter capitalized when the modifier is '*'" do
+        subject.modify_case(string, '*').should eq('Hello World')
       end
 
       it "should return its first parameter unchanged when the modifier is unknown" do
-        @dummy.modify_case(@str, '@').should eq(@str)
+        subject.modify_case(string, '@').should eq(string)
       end
     end
 
-    it "should be able to camelize a string" do
-      @dummy.camelize(@str).should eq('Hello World')
+    describe '#camelcase' do
+      it "should return a camelized version of the string" do
+        subject.camelcase(string).should eq('Hello World')
+      end
     end
   end
 end
